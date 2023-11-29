350e4c5d83d7bb2eec9c3ece6a70119722d7fa5a
csae8092 <peter.andorfer@oeaw.ac.at>
2023-11-29 12:49:18 +0100

    this could work

--- fetch_geonames.py
+++ fetch_geonames.py
@@ -1,32 +1,44 @@
-import requests
 import lxml.etree as ET
-from AcdhArcheAssets.uri_norm_rules import get_normalized_uri 
+from AcdhArcheAssets.uri_norm_rules import get_normalized_uri, get_norm_id
 from acdh_tei_pyutils.tei import TeiReader
 from tqdm import tqdm
 
 
 file = "./data/indices/listplace.xml"
+tei_uri = "https://pmb.acdh.oeaw.ac.at/apis/entities/tei/place/{}"
+
 
 doc = TeiReader(file)
 lookup = dict()
 nsmap = doc.nsmap
-for x in tqdm(doc.any_xpath(".//tei:place[./tei:idno[@type='pmb']]")):
+no_geonames = []
+failed = []
+for x in tqdm(
+    doc.any_xpath(
+        ".//tei:place[./tei:idno[@type='pmb'] and not(./tei:idno[@type='geonames'])]"
+    )
+):
     uri = x.xpath("./tei:idno[@type='pmb']", namespaces=nsmap)[0].text
+    pmb_id = get_norm_id(uri)
+    pmb_uri = tei_uri.format(pmb_id)
     try:
-        r = requests.get(uri)
-    except:
-        print(f"failed to data for {x}")
-        continue
-    data = r.json()
-    uris = data["uris"]
+        place_doc = TeiReader(pmb_uri)
+    except Exception as e:
+        failed.append(x, e)
     try:
-        geonames = [x["uri"] for x in data["uris"] if "geonames" in x["uri"]][0]
-    except:
+        geonames = place_doc.any_xpath(".//idno[@subtype='geonames']")[0]
+    except IndexError:
+        no_geonames.append(uri)
         continue
-    node = ET.Element("{http://www.tei-c.org/ns/1.0}indo")
+    node = ET.Element("{http://www.tei-c.org/ns/1.0}idno")
     node.attrib["type"] = "geonames"
-    node.text = get_normalized_uri(geonames)
+    node.text = get_normalized_uri(geonames.text)
     x.append(node)
 doc.tree_to_file(file)
 
+print("NO geonames")
+print(no_geonames)
 
+print("#####")
+print("failed")
+print(failed)
